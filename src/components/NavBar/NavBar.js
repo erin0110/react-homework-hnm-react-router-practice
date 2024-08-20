d33bef6fa1ff24a3f82775f894d652180b5a41ab
eunji <eunji92@estsoft.com>
2024-08-20 18:56:58 +0900

    hnm프로젝트 추가

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <header>
+      <div className="header-inner">
+        <div className="top"></div>
+        <div className="logo"></div>
+        <div className="nav"></div>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
