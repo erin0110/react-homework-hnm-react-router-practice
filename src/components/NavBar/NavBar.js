1d071aa736ef06043d691a6cf0d6637355eb8bd1
erin0110 <94297941+erin0110@users.noreply.github.com>
2024-08-24 16:14:58 +0900

    모바일 햄버거메뉴 기능 추가

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,14 @@
-import { faMagnifyingGlass, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faMagnifyingGlass,
+  faUser,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useNavigate } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
 import "./NavBar.css";
+import { useState } from "react";
 
 const NavBar = ({ authenticate, setAuthenticate }) => {
   const menuList = [
@@ -24,54 +30,65 @@ const NavBar = ({ authenticate, setAuthenticate }) => {
       navigate(`/?q=${keyword}`);
     }
   };
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    isOpen
+      ? (document.body.style.overflow = "auto")
+      : (document.body.style.overflow = "hidden");
+    setIsOpen((isOpen) => !isOpen);
+  };
 
   return (
     <header>
       <div className="header-inner">
-        <div className="heading">
-          <h1>
-            <a href="/">
-              <img src={logo} alt="" />
-            </a>
-          </h1>
-          <div className="lnb">
-            <div className="sign">
-              {authenticate ? (
-                <div onClick={() => setAuthenticate(false)}>
-                  <FontAwesomeIcon icon={faUser} />
-                  LOGOUT
-                </div>
-              ) : (
-                <div onClick={() => navigate("/login")}>
-                  <FontAwesomeIcon icon={faUser} />
-                  LOGIN
-                </div>
-              )}
-            </div>
-
-            <div className="search">
-              <i>
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
-              </i>
-              <input
-                type="text"
-                placeholder="검색"
-                onKeyPress={(e) => search(e)}
-              />
-            </div>
+        <div className="lnb">
+          <div className="search">
+            <i>
+              <FontAwesomeIcon icon={faMagnifyingGlass} size="1x" />
+            </i>
+            <input
+              type="text"
+              placeholder="Search..."
+              onKeyPress={(e) => search(e)}
+            />
           </div>
+          {authenticate ? (
+            <button className="sign" onClick={() => setAuthenticate(false)}>
+              <FontAwesomeIcon icon={faUser} />
+              LOGOUT
+            </button>
+          ) : (
+            <button className="sign" onClick={() => navigate("/login")}>
+              <FontAwesomeIcon icon={faUser} />
+              LOGIN
+            </button>
+          )}
         </div>
-        <div className="nav">
-          <ul className="menu">
-            {menuList.map((menu, index) => {
-              return (
-                <li key={index}>
-                  <a href="#n">{menu}</a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        <h1 className="logo">
+          <a href="/">
+            <img src={logo} alt="" />
+          </a>
+        </h1>
+        <button className="btn-hamburger" onClick={() => toggleMenu()}>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+        <nav className={isOpen && "open"}>
+          <div className="menu-wrapper">
+            <button className="btn-menu-close" onClick={() => toggleMenu()}>
+              <FontAwesomeIcon icon={faXmark} />
+            </button>
+            <ul className="menu">
+              {menuList.map((menu, index) => {
+                return (
+                  <li key={index}>
+                    <a href="#n">{menu}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </nav>
       </div>
     </header>
   );
--- src/components/ProductCrad/ProductCrad.js
+++ src/components/ProductCrad/ProductCrad.js
@@ -3,18 +3,19 @@ import "./ProductCrad.css";
 
 const ProductCrad = ({ item }) => {
   const navigate = useNavigate();
-  const showDetail = () => {
+  const showDetail = (e) => {
+    e.preventDefault();
     navigate(`/product/${item.id}`);
   };
 
   return (
-    <div className="prd-card" onClick={showDetail}>
+    <a href="#n" className="prd-card" onClick={(e) => showDetail(e)}>
       <img src={item?.img} alt="" />
       <em>{item?.choice && "Conscious choice"}</em>
       <span>{item?.title}</span>
       <strong>₩{item?.price}</strong>
       <i>{item?.new && "신제품"}</i>
-    </div>
+    </a>
   );
 };
 
