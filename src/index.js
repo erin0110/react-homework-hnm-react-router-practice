6c7b06362dd6b7f79fcabf010017b2bfb63e4e7d
eunji <eunji20715@nate.com>
2024-08-25 16:39:38 +0900

    폰트어썸 버그

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from "react";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+config.autoAddCss = false; /* eslint-disable import/first */
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
